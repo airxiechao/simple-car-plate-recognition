c89de79dcea0bd12e3f13eba03f3710d863c5b6a
谢超 <airxiechao@qq.com>
2019-04-18 13:06:41 +0800

    Update carplate.py

--- Mask_RCNN/carplate.py
+++ Mask_RCNN/carplate.py
@@ -111,7 +111,8 @@ class CarplateDataset(utils.Dataset):
         #   'size': 100202
         # }
         # We mostly care about the x and y coordinates of each region
-        annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
+        with open(os.path.join(dataset_dir, "via_region_data.json"), encoding='utf-8') as f:
+            annotations = json.load(f)
         annotations = list(annotations.values())  # don't need the dict keys
 
         # The VIA tool saves images in the JSON even if they don't have any
